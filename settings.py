d50311298f566a20b9327a8d8dbe8453294e9018
Johan Berggren <jbn@nordu.net>
2011-12-09 17:06:25 +0100

    Changed settings

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Django settings for hello project.
+# Django settings for test project.
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'hello.urls'
+ROOT_URLCONF = 'test.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
