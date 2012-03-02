04fc2bc703cbb7d6dbbbd7352aaefaad32d62bf2
Johan Berggren <jbn@nordu.net>
2012-03-02 15:23:18 +0100

    Show current time

--- world/views.py
+++ world/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+import datetime
 
 def index(request):
-	return render_to_response('index.html', {}, RequestContext(request))
+	now = str(datetime.datetime.now())
+	return render_to_response('index.html', {'now': now}, RequestContext(request))
 
